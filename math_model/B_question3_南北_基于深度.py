69809116e9db80cc075d2c12af4e262b62a77ef2
胡健 <22373058@buaa.edu.cn>
2023-09-10 06:25:15 +0800

    变更

--- math_model/B_question3_南北_基于深度.py
+++ math_model/B_question3_南北_基于深度.py
@@ -4,15 +4,18 @@ def fun(h):
     x = h / (math.tan(1.5 * math.pi / 180) + math.cos(61.5 * math.pi / 180) / (math.cos(1.5 * math.pi / 180) * math.sin(60 * math.pi / 180)))
     return x
 
-h = 207.0
-n = 0
-x = 0
+def main():
+    h = 207.0
+    n = 0
+    x = 0
+    while h - x * 0.1 * math.tan(1.5 * math.pi / 180) > 13.0:
+        n += 1
+        x = fun(h)
+        x = x / math.cos(58.5 * math.pi / 180) * (math.cos(61.5 * math.pi / 180) + math.cos(58.5 * math.pi / 180))
+        h = h - (x * 0.9 * math.tan(1.5 * math.pi / 180))
 
-while h - x * 0.1 * math.tan(1.5 * math.pi / 180) > 13.0:
-    n += 1
-    x = fun(h)
-    x = x / math.cos(61.5 * math.pi / 180) * (math.cos(61.5 * math.pi / 180) + math.cos(58.5 * math.pi / 180))
-    h -= (x * 0.9 * math.tan(1.5 * math.pi / 180))
+    print(n)
+    print(n * 1852 * 2)
 
-print(n)
-print(n * 1852 * 2)
+if __name__ == "__main__":
+    main()
